51b32c727a03f480e6ca8dab8131d2ec7de202de
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-09 12:02:44 -0500

    Implement Computer and QueryBuilder classes with configuration display and query methods

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors';
 /**
  * ! Patrón Builder:
  * Es un patrón de diseño creacional que nos permite construir objetos complejos
@@ -12,3 +13,72 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+
+class Computer {
+    public cpu = 'cpu - not defined';
+    public ram = 'ram - not defined';
+    public storage = 'storage- not defined';
+    public gpu?: string;
+
+    displayConfiguration() {
+        console.log(
+            `Configuration: 
+            CPU: ${this.cpu}
+            RAM: ${this.ram}
+            Storage: ${this.storage}
+            GPU: ${this.gpu ? this.gpu : 'not defined'}`
+        )
+    }
+
+
+}
+
+class ComputerBuilder {
+
+    private computer: Computer;
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRAM(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+
+    setGPU(gpu: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(): Computer {
+        return this.computer
+    }
+
+}
+
+
+function main() {
+    const basicComputer = new ComputerBuilder().setCPU('Intel Core i7').setRAM('16GB').setStorage('256GB').build();
+
+    console.log('%cComputer basic configuration:', COLORS.blue);
+    basicComputer.displayConfiguration();
+
+    const gameComputer = new ComputerBuilder().setCPU('AMD Ryzen 5').setRAM('32GB').setStorage('512GB').setGPU('NVIDIA GeForce RTX 3080').build();
+    console.log('%cComputer game configuration:', COLORS.violet);
+    gameComputer.displayConfiguration();
+
+}
+
+main()
+
