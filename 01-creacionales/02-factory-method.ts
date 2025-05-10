334e4ee0a1cca609ae2f282d1858b2e16adc3103
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-09 20:39:17 -0500

    Implement Hamburger and Report classes with preparation and generation methods

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,81 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors"
+
+interface Hamburger {
+    prepare(): void
+}
+
+
+class ChickenHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cPollo', COLORS.yellow)
+    }
+}
+
+class BeefHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cRes', COLORS.red)
+    }
+}
+class BeanHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cFrijoles', COLORS.green)
+    }
+}
+
+abstract class Restaurant {
+    abstract createHamburger(): Hamburger
+    orderHamburger(): void {
+        const hamburger = this.createHamburger()
+        hamburger.prepare()
+    }
+
+}
+
+
+class ChickenRestaurant extends Restaurant {
+    override createHamburger(): Hamburger {
+        return new ChickenHamburger()
+    }
+}
+
+class BeefRestaurant extends Restaurant {
+    override createHamburger(): Hamburger {
+        return new BeefHamburger()
+    }
+}
+class BeanRestaurant extends Restaurant {
+    override createHamburger(): Hamburger {
+        return new BeanHamburger()
+    }
+}
+
+
+function main() {
+
+
+    let restaurant: Restaurant;
+
+    const burgerType = prompt('¿Qué tipo de hamburguesa quieres? (chicken/beef/bean)')
+
+    switch (burgerType) {
+        case 'chicken':
+            restaurant = new ChickenRestaurant();
+            break;
+        case 'beef':
+            restaurant = new BeefRestaurant();
+            break;
+        case 'bean':
+            restaurant = new BeanRestaurant()
+            break;
+        default:
+            throw new Error('Opción no válida')
+    }
+
+    restaurant.orderHamburger();
+}
+
+main()
