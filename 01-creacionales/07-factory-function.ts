866a350f566f3f22246491f2753435d116b385c2
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-10 22:47:08 -0500

    Implement logger factory function with color-coded messages for different log levels

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,33 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+import { COLORS } from "../helpers/colors"
+
+
+type Language = 'es' | 'en' | 'fr'
+
+//i18n
+function createGreeter(lang: Language) {
+    return (name: string) => {
+        const message = {
+            es: `Hola, %c${name}`,
+            en: `Hello, %c${name}`,
+            fr: `Bonjour, %c${name}`
+        }
+        return console.log(message[lang], COLORS.red)
+    }
+
+}
+
+function main(){
+    const spanishGreeter= createGreeter('es')
+    const englishGreeter= createGreeter('en')
+    const frenchGreeter= createGreeter('fr')
+
+    spanishGreeter('Camilo')
+    englishGreeter('Alis')
+    frenchGreeter('Pierre')
+}
+
+main()
