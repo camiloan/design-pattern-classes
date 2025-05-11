a13a34af23efe3b9fedebea9b5920b25497e29c7
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-11 01:24:25 -0500

    Implement Bridge pattern with Ability and Notification classes; add color-coded logging for actions

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Patrón Bridge
  * Este patrón nos permite desacoplar una abstracción de su implementación,
@@ -9,3 +10,77 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+
+interface Ability {
+    use(): void
+}
+
+class SwordAttack implements Ability {
+    use(): void {
+        console.log('Ataca con una %cespada ferozmente', COLORS.blue)
+    }
+}
+class MagicSpell implements Ability {
+    use(): void {
+        console.log('Lanza un %chechizo mágico poderoso', COLORS.purple)
+    }
+}
+class AxeAttack implements Ability {
+    use(): void {
+        console.log('Lanza un %chachazo desgarrador', COLORS.red)
+    }
+}
+class FireballSpell implements Ability {
+    use(): void {
+        console.log('Lanza una  %cbola de fuegocmágico poderoso', COLORS.yellow)
+    }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability) {
+        this.ability = ability
+    }
+
+    setAbility(ability: Ability): void {
+
+        this.ability = ability
+    }
+
+    abstract performAbility(): void;
+
+}
+
+class Warrior extends Character {
+    override performAbility(): void {
+        console.log('\nEl guerrero esta listo para luchar');
+        this.ability.use()
+    }
+}
+
+class Mage extends Character {
+    override performAbility(): void {
+        console.log('\nEl mago prepara su hechizo');
+        this.ability.use()
+    }
+
+}
+
+function main() {
+    const warrior = new Warrior(new SwordAttack())
+
+    warrior.performAbility()
+
+    warrior.setAbility(new AxeAttack())
+    warrior.performAbility()
+
+    const mage = new Mage(new FireballSpell())
+    mage.performAbility()
+    mage.setAbility(new MagicSpell())
+    mage.performAbility()
+
+}
+
+main()
