a423e128cc3aa03e711bb1c896da6307a0eaf30e
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-10 19:37:43 -0500

    Implement Singleton pattern with DragonBalls and DatabaseConnection classes, and add ConfigManager for configuration management

--- 01-creacionales/06-singleton.ts
+++ 01-creacionales/06-singleton.ts
@@ -9,3 +9,64 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import { COLORS } from "../helpers/colors";
+
+class DragonBalls {
+    private static instance: DragonBalls;
+    private ballsCollected: number
+
+
+    private constructor() {
+        this.ballsCollected = 0
+    }
+
+    public static getInstance(): DragonBalls {
+        if (!DragonBalls.instance) {
+            DragonBalls.instance = new DragonBalls();
+            console.log('%cLas esferas del Dragón han sido creadas', COLORS.green)
+        }
+        return DragonBalls.instance;
+    }
+
+    summonShenlong() {
+        if (this.ballsCollected === 7) {
+            console.log('Shenlog ha sido invocado, Pide tu deseo!');
+            this.ballsCollected = 0;
+            return;
+        }
+
+        console.log(`Aún faltan ${7 - this.ballsCollected} pelotas para invocar a Shenlong`)
+    }
+
+    collectBall(): void {
+        if (this.ballsCollected < 7) {
+            this.ballsCollected++;
+            console.log(`Esfera recolectada. Total de esferas: ${this.ballsCollected}`)
+            return;
+        }
+
+        console.log('\nYa se han recolectado las 7 esferas del Drágon! Invoca a Shenlong')
+    }
+
+}
+
+function main() {
+    const gokuDragonBalls = DragonBalls.getInstance()
+    gokuDragonBalls.collectBall()
+    gokuDragonBalls.collectBall()
+    gokuDragonBalls.collectBall()
+
+    gokuDragonBalls.summonShenlong()
+
+    const vegetaDragonBalls = DragonBalls.getInstance();
+    vegetaDragonBalls.collectBall();
+    vegetaDragonBalls.collectBall();
+    vegetaDragonBalls.collectBall();
+    vegetaDragonBalls.collectBall();
+    gokuDragonBalls.summonShenlong()
+
+    vegetaDragonBalls.summonShenlong()
+}
+
+main()
--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -12,24 +12,43 @@ import { COLORS } from '../helpers/colors.ts';
 
 class DatabaseConnection {
   private static instance: DatabaseConnection;
-  private connected: boolean = false;
+  private connected = false;
 
   // Constructor privado para evitar instancias directas
-  private constructor() {}
+  private constructor() { }
 
   // Método estático para obtener la instancia única
   public static getInstance(): DatabaseConnection {
-    // Completar: implementar el patrón Singleton
-    throw new Error('Method not implemented.');
+
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+      console.log('%cConectado a la base de datos', COLORS.blue)
+    }
+
+    return DatabaseConnection.instance
   }
 
   // Método para conectar a la base de datos
   public connect(): void {
+    if (this.connected) {
+      console.log('%cYa estabamos conectado a la base de datos', COLORS.red)
+      return;
+    }
+
+    this.connected = true
+    console.log('%cNueva conexión a la base de datos', COLORS.yellow)
+
     // Completar: si no está conectado, mostrar mensaje de conexión
   }
 
   // Método para desconectar de la base de datos
   public disconnect(): void {
+    if (this.connected) {
+      console.log('%cDesconectamos la conexión de base de datos', COLORS.brown)
+      this.connected = false
+      return;
+    }
+    console.log('%cNo hay una conexión activa', COLORS.red)
     // Completar: desconectar y mostrar mensaje de desconexión
   }
 }
@@ -44,6 +63,7 @@ function main() {
 
   console.log('Son iguales:', db1 === db2); // Debería mostrar true
 
+  db1.disconnect(); // Debería cerrar la conexión
   db1.disconnect(); // Debería cerrar la conexión
 
   db2.connect(); // Ahora debería conectar de nuevo, ya que se cerró la anterior
